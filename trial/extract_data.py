13f7fe4e3f9b67bf2d46f63c07c9de83c68085c4
shaniba3 <noga2015>
2024-11-12 19:10:33 +0200

    added data json files and data extraction script

---
+++ trial/extract_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+import json
+
+from utils import json_to_df, get_most_common_word_simple
+
+
+base_repo_dir = r"C:\Users\User\Documents\Ruppin\age_prediction_project_repo"  # replace for your own
+train_path = os.path.join(base_repo_dir, "data_files", "train.json")
+
+# extracting train data into a df:
+train_df = json_to_df(json_file=train_path)
+print(train_df.shape)
+print(train_df.head((2)))
+
+
+# Generate a basic feature DataFrame for the first 1000 posts
+basic_feature_df = train_df[["age", "gender", "post"]].iloc[:1000]
+# Ensure 'post_index' is correctly copied
+basic_feature_df["post_index"] = basic_feature_df.index
+# Calculate word count for each post
+basic_feature_df["word_count"] = basic_feature_df["post"].apply(lambda x: len(x.split()) if pd.notnull(x) else 0)
+# Find the most common word in each post
+basic_feature_df["most_common_word"] = basic_feature_df["post"].apply(get_most_common_word_simple)
+
+print(basic_feature_df.shape)
+print(basic_feature_df.head())
---
+++ utils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+from collections import Counter
+
+
+def json_to_dict(json_file: str) -> dict:
+    """
+    Reads a json file and return a dict
+    :param json_file: full path to json file
+    :return: dict
+    """
+    with open(json_file,'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+def json_to_df(json_file: str) -> pd.DataFrame:
+    """
+    Reads a json file and return a dataframe
+    :param json_file: full path to json file
+    :return: a dataframe
+    """
+    return pd.read_json(json_file)
+
+
+def get_most_common_word_simple(post: str) -> str:
+    """
+    Returns the most common word in a post. note: this function is very simplistic
+    :param post: the post text
+    :return: the most common word. if there are several words with the same frequency, it will return the first one
+    """
+    if pd.isnull(post) or not post.strip():
+        return None
+    words = post.split()
+    most_common = Counter(words).most_common(1)
+    return most_common[0][0] if most_common else None
