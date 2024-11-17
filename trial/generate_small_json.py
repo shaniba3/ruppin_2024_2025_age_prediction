92fef7de1849fe37b084eaa5787c79ccb55f45fe
shaniba3 <noga2015>
2024-11-17 23:26:23 +0200

    removed large data files and generated a small data file instead

---
+++ trial/generate_small_json.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import random
+import json
+
+from utils import json_to_dict
+
+base_repo_dir = r"C:\Users\User\Documents\Ruppin\age_prediction_project_repo"  # replace for your own
+train_path = os.path.join(base_repo_dir, "data_files", "train.json")
+
+train_data = json_to_dict(train_path)
+
+print(len(train_data))
+
+smaller_train_data = random.sample(train_data, 10000)
+print(len(smaller_train_data))
+
+smaller_train_data_path = os.path.join(base_repo_dir, "data_files", "train_10000.json")
+with open(smaller_train_data_path, "w") as fp:
+    json.dump(smaller_train_data, fp)
+
+small_train_data_2 = json_to_dict(smaller_train_data_path)
+
